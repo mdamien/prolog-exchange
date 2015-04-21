c3abc42b11df09f2b67860f28a8d3348f10cc52d
Damien <damien@dam.io>
2015-04-21 17:59:04 +0200

    affiche marchandises

--- projet.pl
+++ projet.pl
@@ -1,7 +1,15 @@
 %affiche_plateau(+Plateau)
-%affiche_plateau([[1,2], [1,2],[],[],[]]).
+%affiche_plateau([[[mais,ble],[ble,sucre]], [1,2],[],[],[]]).
 
-%gprolog --init-goal "[projet],affiche_plateau([[1,2], [1,2],[],[],[]]),halt"
+%gprolog --init-goal "[projet],affiche_plateau([[[mais,ble],[ble,sucre]], [],[],[],[]]),halt"
+
+afficher_tas_marchandise([]):-nl.
+afficher_tas_marchandise([H|T]):-write(H),write(','),afficher_tas_marchandise(T).
+
+affiche_marchandise([H|T]):-write('tas:'),nl,afficher_tas_marchandise(H),nl,affiche_marchandise(T),nl.
 
 affiche_plateau([Marchandises, Bourse, 
-        PositionTrader,ReserveJoueur1,ReserveJoueur2]):-write('plateau yeah'),nl.
+        PositionTrader,ReserveJoueur1,ReserveJoueur2]):-
+    write('--MARCHANDISES--'),nl,
+    affiche_marchandise(Marchandises),
+    nl.
