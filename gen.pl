eaed73a2fd3203ec528f881e3eadd837a2ec290f
PICHOU Kyâne <kyane.pichou@etu.utc.fr>
2015-04-21 18:39:26 +0200

    Generation bourse

---
+++ gen.pl
@@ -0,0 +1,14 @@
+%Variables globales
+min(0).
+max(6).
+
+%gen_bourse(?Bourse)
+gen_bourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]]).
+
+%gen_march(?Marchandises)
+
+%gen_pile(?Pile)
+%gen_pile([A,B,C,D]) :- random(
+
+%gen_elem(?Element)
+%gen_elem(X) :- random(1,6,Y).
