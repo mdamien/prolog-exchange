71274be6c82793bec3a967eea38625179730e4bd
Damien <damien@dam.io>
2015-05-19 18:00:58 +0200

    :fire: :fire: :fire: :rocket: On peut jouer pour de faux!

--- jouer.pl
+++ jouer.pl
@@ -1,15 +1,32 @@
+% gprolog --init-goal "[jouer],jouer"
+
 :- include(affichage). 
 :- include(gen). 
 :- include(coup). 
 
+entrer_nombre(X):-read(X),number(X),X > -1,X < 4,!.
+entrer_nombre(X):-write('Nombre invalide, re-essaye: '),entrer_nombre(X).
+
+joueur_suivant(j1,j2).
+joueur_suivant(j2,j1).
+
+cls :- put(27),write('[2J'). 
+
 jouer:-
     plateau_depart(P),
+    tour(j1,P).
+
+tour(J,P):-
+    cls,
     affiche_plateau(P),
-    write('TOUR DE J1'),nl,
+    write('TOUR DE '),write(J),nl,
     write('Avancer de combien ? '),
-    read_number(Choix),
-    write(Choix),nl,
-    write('Que prendre entre X et Y ? '),
-    read_token(Choix2),
-    write(Choix2),nl,
+    entrer_nombre(Depl),
+    write('avance de '),write(Depl),nl,
+    write('Que prendre entre xxx et yyy ? '),
+    read(Choix),
+    write(J),write(' avance de '),write(Depl),write('et prend '),write(Choix),nl,
+    %jouer_coup(J, Depl, Choix, P),
+    joueur_suivant(J,Jsuivant),
+    tour(Jsuivant, P),
     !.
