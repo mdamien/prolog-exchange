0d1b1fb79049ec81c5957e0ac0e824b52eaa9290
Damien <damien@dam.io>
2015-06-02 17:21:02 +0200

    use jouer.pl

--- jouer.pl
+++ jouer.pl
@@ -7,22 +7,22 @@
 entrer_nombre(X):-read(X),number(X),X > -1,X < 4,!.
 entrer_nombre(X):-write('Déplacement invalide, re-essaye: '),entrer_nombre(X).
 
-joueur_suivant(j1,j2).
-joueur_suivant(j2,j1).
+joueur_suivant('j1','j2').
+joueur_suivant('j2','j1').
 
 cls :- put(27),write('[2J'). 
 
 jouer:-
     plateau_depart(P),
-    tour(j1,P).
+    tour('j1',P).
 
 top_pile(Index,M,Top):-
     nth(Index,M,Pile),
     last(Pile,Top).
 
 choix(M,Trader,Choix1,Choix2):-
-    avancer_mod(Trader, -1, M, P1),
-    avancer_mod(Trader, 1, M, P2),
+    bouger_trader(M, Trader, -1, P1),
+    bouger_trader(M, Trader, 1, P2),
     top_pile(P1,M,Choix1),
     top_pile(P2,M,Choix2).
 
@@ -41,8 +41,7 @@ tour(J,[M, B, Trader, RJ1, RJ2]):-
     entrer_nombre(Depl),
     write('avance de '),write(Depl),nl,
     %avancer()
-    avancer_mod(Trader, Depl, M, TraderPred),
-    write('TraderPred:'),write(TraderPred),nl,
+    bouger_trader(M,Trader,Depl,TraderPred),
     cls,
     choix(M, TraderPred, Choix1,Choix2),
 
@@ -56,7 +55,7 @@ tour(J,[M, B, Trader, RJ1, RJ2]):-
 
     read(Choix),
     write(J),write('prend '),write(Choix),nl,
-    %prendre dans pile()
+    %jouer_coup([M, B, Trader, RJ1, RJ2], [J,Depl,Choix,Choix], NouveauPlateau)
     joueur_suivant(J,Jsuivant),
-    tour(Jsuivant, [M, B, TraderPred, RJ1, RJ2]),
+    tour(Jsuivant, NouveauPlateau),
     !.
